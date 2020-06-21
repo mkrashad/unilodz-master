8f4fbdd2ad2c171b03e53e4e2c9e80562ba1fef6
Rashad Mahmudov <mkrashad25@gmail.com>
2020-06-21 20:16:37 +0200

    Completed Diffie HellmanAlgorithm

--- first/summer/Advance Encryption Methods/Projects/diffie_hellman/diffie_hellman.py
+++ first/summer/Advance Encryption Methods/Projects/diffie_hellman/diffie_hellman.py
@@ -1,27 +1,45 @@
-def prime(num):
-    if num > 1:
-        for i in range(2, num):
-            if (num % i) == 0:
-                return None
+import random
+from math import pow
+
+
+def is_prime(number):
+    if number > 1:
+        for i in range(2, number):
+            if (number % i) == 0:
+                return False
                 break
         else:
-            return num
+            return True
     else:
-        return None
+        return False
 
 
-def safe_prime(p):
-    if p == None:
-        print("isn't prime")
-    else:
-        safe_prime = (2*p) + 1
-        print(safe_prime)
+def safe_prime(start, end):
+    new_prime = []
+    primes = [i for i in range(start, end) if is_prime(i)]
+    for p in primes:
+        check_prime = (2*p) + 1
+        if is_prime(check_prime):
+            new_prime.append(p)
+    random_safe_prime = random.choice(new_prime)
+    return random_safe_prime
 
 
 def main():
-    n = int(input("Write any number "))
-    p = prime(n)
-    safe_prime(p)
+    start = int(input("Enter start value "))
+    end = int(input("Enter end value "))
+    p = safe_prime(start, end)
+    g = random.randint(2, p-2)
+    try:
+        v = pow(g, (p-1)/2) % p
+    except OverflowError:
+        v = float('inf')
+    # v = g**((p-1)/2) % p
+    if v == 1:
+        print("We are failed, play this game again")
+    else:
+        print("Diffie-Hellman parameters is: (p,g)", p, g)
+
 
 
 if __name__ == "__main__":
