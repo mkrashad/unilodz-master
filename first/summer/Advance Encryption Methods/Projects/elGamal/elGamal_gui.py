2594feb530f74db2a886b3e5f6c0cedbd39b715b
Rashad <mkrashad25@gmail.com>
2020-07-06 14:01:23 +0200

    Added try except block and dunder name function

--- first/summer/Advance Encryption Methods/Projects/elGamal/elGamal_gui.py
+++ first/summer/Advance Encryption Methods/Projects/elGamal/elGamal_gui.py
@@ -4,14 +4,11 @@ gi.require_version("Gtk", "3.0")
 
 
 class MyWindow(Gtk.Window):
-    # Constructor
     def __init__(self):
-        # Window Title
-        Gtk.Window.__init__(self, title="Rashad's Cipher 1.0")
-        # Window Size
-        Gtk.Window.set_default_size(self, 600, 200)
-        # Window Border
-        self.set_border_width(20)
+        # Window Settings
+        Gtk.Window.__init__(self, title="Rashad's Cipher 1.1.1")
+        Gtk.Window.set_default_size(self, 500, 200)
+        self.set_border_width(10)
 
         # Box Layout
         self.grid = Gtk.Grid()
@@ -19,54 +16,44 @@ class MyWindow(Gtk.Window):
         self.grid.set_row_spacing(10)
         self.add(self.grid)
 
-        # Label Title 1
+        # Title 1
         self.label_title1 = Gtk.Label(
             "Please write safe prime, generator and cipher text")
         self.grid.attach(self.label_title1, 0, 0, 10, 2)
 
-        # Label Safe Prime
+        # Safe Prime
         self.label_prime = Gtk.Label("Safe prime: ")
         self.grid.attach(self.label_prime, 0, 3, 1, 2)
-
-        # Entry Safe Prime
         self.entry_prime = Gtk.Entry()
         self.grid.attach(self.entry_prime, 1, 3, 1, 2)
 
-        # Label Generator
+        # Generator
         self.label_generator = Gtk.Label("Generator: ")
         self.grid.attach(self.label_generator, 2, 3, 1, 2)
-
-        # Entry Generator
         self.entry_generator = Gtk.Entry()
         self.grid.attach(self.entry_generator, 3, 3, 1, 2)
 
-        # Label Cipher Text
+        # Cipher Text
         self.label_cipher = Gtk.Label("Cipher Text: ")
         self.grid.attach(self.label_cipher, 4, 3, 1, 2)
-
-        # Entry Cipher Text
         self.entry_cipher = Gtk.Entry()
         self.grid.attach(self.entry_cipher, 5, 3, 1, 2)
 
-        # Label Title 2
+        # Title 2
         self.label_title2 = Gtk.Label(
             "Please provide Alice's public key and Bob's private key"
         )
         self.grid.attach(self.label_title2, 0, 5, 10, 2)
 
-        # Label Alice public key
+        # Alice public key
         self.label_alice = Gtk.Label("Public Key: ")
         self.grid.attach(self.label_alice, 0, 7, 1, 2)
-
-        # Entry Alice public key
         self.entry_alice = Gtk.Entry()
         self.grid.attach(self.entry_alice, 1, 7, 1, 2)
 
-        # Label Bob private key
+        # Bob private key
         self.label_bob = Gtk.Label("Private Key: ")
         self.grid.attach(self.label_bob, 2, 7, 1, 2)
-
-        # Entry Bob private key
         self.entry_bob = Gtk.Entry()
         self.grid.attach(self.entry_bob, 3, 7, 1, 2)
 
@@ -107,50 +94,64 @@ class MyWindow(Gtk.Window):
         return B, ss, x, y
 
     def main(self, widget):
-        # Get Safe Prime
-        # p = 16487
-        p = int(self.entry_prime.get_text())
-        # Get Generator
-        # g = 14
-        g = int(self.entry_generator.get_text())
-        # Get Cipher text
-        # c1 = 15989
-        c1 = int(self.entry_cipher.get_text())
-        # Get Alice Public key
-        # A = 2454
-        A = int(self.entry_alice.get_text())
-        # Get Bob Private key
-        # b = 3259
-        b = int(self.entry_bob.get_text())
-        # Get Bob's public key, shared secret, inverse modular and original pin
-        B, ss, x, y = self.compute(p, g, c1, A, b)
-        # B = 2334
-        # ss = 4459
-        # x = 9484
-        # y =  8737
-        result = "Bob's public key is: " + \
-            str(B) + "\n" + "Shared secret is: " + str(ss) + "\n" + "Inverse modular of ss is: " + \
-            str(x) + "\n" + "Debit/Credit card's PIN number is: " + str(y)
-        print(result)
+        try:
+            # Get Safe Prime
+            # p = 16487
+            p = int(self.entry_prime.get_text())
+            # Get Generator
+            # g = 14
+            g = int(self.entry_generator.get_text())
+            # Get Cipher text
+            # c1 = 15989
+            c1 = int(self.entry_cipher.get_text())
+            # Get Alice Public key
+            # A = 2454
+            A = int(self.entry_alice.get_text())
+            # Get Bob Private key
+            # b = 3259
+            b = int(self.entry_bob.get_text())
+            # Get Bob's public key, shared secret, inverse modular and original pin
+            B, ss, x, y = self.compute(p, g, c1, A, b)
+            # B = 2334
+            # ss = 4459
+            # x = 9484
+            # y =  8737
+            result = "Bob's public key is: " + \
+                str(B) + "\n" + "Shared secret is: " + str(ss) + "\n" + "Inverse modular of ss is: " + \
+                str(x) + "\n" + "Debit/Credit card's PIN number is: " + str(y)
+            print(result)
 
-        # Dialog Box
-        dialog = Gtk.MessageDialog(
-            self,
-            0,
-            Gtk.MessageType.INFO,
-            Gtk.ButtonsType.OK,
-            "The ciphertext decrypted successfully"
-        )
-        dialog.format_secondary_text(
-            result
-        )
-        dialog.run()
-        print("INFO dialog closed")
+            # Dialog Box
+            dialog = Gtk.MessageDialog(
+                self,
+                0,
+                Gtk.MessageType.INFO,
+                Gtk.ButtonsType.OK,
+                "The ciphertext decrypted successfully"
+            )
+            dialog.format_secondary_text(
+                result
+            )
+            dialog.run()
+            print("INFO dialog closed")
 
-        dialog.destroy()
+            dialog.destroy()
+        except ValueError:
+          # Dialog Box
+            dialog = Gtk.MessageDialog(
+                self,
+                0,
+                Gtk.MessageType.ERROR,
+                Gtk.ButtonsType.CLOSE,
+                "Please enter all fields and only integers"
+            )
+            dialog.run()
+            print("Please enter all fields")
+            dialog.destroy()
 
 
-win = MyWindow()
-win.connect("destroy", Gtk.main_quit)
-win.show_all()
-Gtk.main()
+if __name__ == "__main__":
+    win = MyWindow()
+    win.connect("destroy", Gtk.main_quit)
+    win.show_all()
+    Gtk.main()
